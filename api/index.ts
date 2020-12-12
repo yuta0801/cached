945f1a0441c8959c9d48a822bcb390e40ed3fd3f
yuta0801 <yuta030801@gmail.com>
2020-12-12 09:44:11 +0900

    forward headers

--- api/index.ts
+++ api/index.ts
@@ -4,16 +4,16 @@ import { ms } from "https://deno.land/x/ms@v0.1.0/ms.ts";
 
 export default async (req: ServerRequest) => {
   const { pathname, search } = new URL(req.url, "http://localhost");
-  console.log(req.url, pathname, search);
+
   const age = (ms(pathname.slice(1)) as number / 1000).toFixed();
   const url = decodeURIComponent(search.slice(1));
 
-  const headers = new Headers({
-    "Cache-Control": `max-age=${age}, public`,
-  });
-
-  const body = (await fetch(url)).body!;
+  const res = await fetch(url);
+  const body = res.body!;
   const reader = readerFromStreamReader(body.getReader());
+
+  const headers = res.headers;
+  headers.set("Cache-Control", `max-age=${age}, public`);
 
   req.respond({ headers, body: reader });
 };
